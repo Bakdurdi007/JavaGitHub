970dce10046f341a450eb5f74b116f5d70221127
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-01-22 17:38:44 +0500

    Test5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" Hello awzesxrtfvgb! ");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print(" Enter a: ");
+        int a = scanner.nextInt();
+
+        System.out.print(" Enter b: ");
+        int b = scanner.nextInt();
+
+        int S = a + b;
+
+        System.out.println(" S = a + b = " + S);
     }
 }
