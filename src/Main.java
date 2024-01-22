a9c595f0ad83004675602a45926008026e53ba7f
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-01-22 17:47:42 +0500

    Test13

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,5 @@ public class Main {
 
         System.out.println(" S = a + b = " + (a + b));
         System.out.println(" S = a - b = " + (a - b));
-        System.out.println(" S = a * b = " + (a * b));
-        System.out.println(" S = a / b = " + (a / b));
     }
 }
