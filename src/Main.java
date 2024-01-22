adb6989e9926f9325b0bc21480d3c8403fa6f53b
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-01-22 17:53:04 +0500

    Bakdurdi

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,9 @@ public class Main {
 
         System.out.println(" S = a + b = " + (a + b));
         System.out.println(" S = a - b = " + (a - b));
-        System.out.println(" S = a * b = " + (a * b));
-        System.out.println(" S = a / b = " + (a / b));
 
-        System.out.println(" Hellooooooooooooooooooo");
-        System.out.println("MuhammadSolih");
+
+
+        System.out.println(" asdfghjk hcbhixhbfgbnjbfijbnj");
     }
 }
