c42da70d46c0bc5c08d0acb78cf0627b3c29c4d4
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-01-22 17:22:19 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*******************************************************
+ * Author: Davletov Bakdurdi
+ * Date: 22-January. 2024-year.
+ * Week day: Monday
+ * Time: 5:21 p.m.
+ *******************************************************///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Bu yangi loyiha. ");
+    }
+}
